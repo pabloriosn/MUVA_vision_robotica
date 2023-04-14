b858df2db9afb1ae9d6192f24e7a5a3cb894f69c
pablo <pablorios@edu.uah.es>
2023-04-14 14:21:49 +0200

    Fuction get_points_interest

--- 3d_reconstruction/algorithm.py
+++ 3d_reconstruction/algorithm.py
@@ -7,7 +7,7 @@ import time
 
 
 class Reconstruction:
-    def __init__(self, image_left: np.ndarray, image_right: np.ndarray):
+    def __init__(self, image_left: np.ndarray, image_right: np.ndarray, verbose: bool = False):
         self.image_left = image_left
         self.image_right = image_right
 
@@ -15,7 +15,9 @@ class Reconstruction:
         self.camera_left = HAL.getCameraPosition('left')
         self.camera_right = HAL.getCameraPosition('right')
 
-    def pointsinterest(self) -> (np.ndarray, np.ndarray):
+        self.verbose = verbose
+
+    def get_points_interest(self) -> (np.ndarray, np.ndarray):
         edges_left = cv2.Canny(cv2.cvtColor(self.image_left, cv2.COLOR_BGR2GRAY), 100, 200)
         edges_right = cv2.Canny(cv2.cvtColor(self.image_right, cv2.COLOR_BGR2GRAY), 100, 200)
 
@@ -24,23 +26,23 @@ class Reconstruction:
     def algorithm(self):
 
         # Get points of interest
-        points_left, points_right = self.pointsinterest()
+        points_left, points_right = self.get_points_interest()
         print(f"Number of points of interest {points_left.shape[0]}")
 
         for y, x in points_left:
 
-            c_left = np.append(self.camera_left, 1)
-            p_left = HAL.backproject('left', HAL.graficToOptical('left', [x, y, 1]))
-            print(f"camera left position: {c_left}, point left: {p_left}")
-
-            cv2.circle(self.image_left, (x, y), 2, (0, 255, 0), 1)
+            if self.verbose:
+                cv2.circle(self.image_left, (x, y), 2, (0, 255, 0), 1)
 
         return self.image_left, cv2.Canny(cv2.cvtColor(self.image_left, cv2.COLOR_BGR2GRAY), 100, 200)
 
 
 while True:
-    reconstruction = Reconstruction(HAL.getImage('left'), HAL.getImage('right'))
+    reconstruction = Reconstruction(HAL.getImage('left'), HAL.getImage('right'), verbose=True)
 
     out_left, out_right = reconstruction.algorithm()
 
     GUI.showImages(out_left, out_right, True)
+
+    print("hola")
+    time.sleep(10)
